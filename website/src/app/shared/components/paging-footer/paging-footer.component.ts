bbc39b514c69e9510de2aac6eb3365bf25e34cfa
adel ibrahim <adelanwer12@yahoo.com>
2021-02-05 17:36:31 +0200

    End Section 9

---
+++ website/src/app/shared/components/paging-footer/paging-footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-paging-footer',
+  templateUrl: './paging-footer.component.html',
+  styleUrls: ['./paging-footer.component.css']
+})
+export class PagingFooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
